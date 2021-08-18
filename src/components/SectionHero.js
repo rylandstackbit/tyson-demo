ebbad3d93daafc7cdce5e221d7b227a19680f0c3
Stackbit Code Editor <projects@stackbit.com>
2021-08-18 18:17:39 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -76,7 +76,7 @@ export default function SectionHero(props) {
                             <img src={withPrefix(image)} alt={imageAlt} />
                         </div>
                     )}
-                    <div className="cell block-content order-content" style={{textAlign: textAlign, maxWidth: textBlockWidth}}>
+                    <div className="cell block-content order-content" style={{textAlign: textAlign, maxWidth: textBlockWidth, fontWeight: `bold`}}>
                         {title && <h2 className="block-title" style={{color: titleColor, fontSize: `5rem`}}>{title}</h2>}
                         {content && (
                             <div style={{color: subtitleColor, fontSize: `2.5rem`}}>

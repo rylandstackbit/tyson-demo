5a0a8951bba363cc5c600a5b5ee7c8ab5a8f3fc8
Stackbit Code Editor <projects@stackbit.com>
2021-08-18 18:24:52 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -61,7 +61,7 @@ export default function Action(props) {
             // })}
              style={{backgroundColor: backgroundColor, borderColor: borderColor, borderRadius: borderRadius, padding: `${paddingVertical} ${paddingHorizontal}`}}
         >
-            <span className="" style={{color: textColor, fontSize: `1rem`}}>{label}</span>
+            <span className="" style={{color: textColor, fontSize: `1.3rem`}}>{label}</span>
         </Link>
     );
 }
